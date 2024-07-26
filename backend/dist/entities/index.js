d43fa117c01326d985b2b472239fd8d500efaee1
Michael Shields <mr.m.p.shields@gmail.com>
2024-07-26 13:02:58 +0100

    chore: Add backend > dist dir to backend > .gitignore file

--- backend/dist/entities/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Lesson = exports.Tutorial = void 0;
-const Tutorial_1 = require("./Tutorial");
-Object.defineProperty(exports, "Tutorial", { enumerable: true, get: function () { return Tutorial_1.Tutorial; } });
-const Lesson_1 = require("./Lesson");
-Object.defineProperty(exports, "Lesson", { enumerable: true, get: function () { return Lesson_1.Lesson; } });
