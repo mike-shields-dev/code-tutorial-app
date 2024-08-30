4e4b47d9762ea4929400f081c36ca430c0a41628
Michael Shields <mr.m.p.shields@gmail.com>
2024-08-30 10:06:23 +0100

    refactor: Move dummy data to shared dir

--- backend/scripts/seedDb.ts
+++ backend/scripts/seedDb.ts
@@ -1,6 +1,7 @@
 import { DataSource } from "typeorm";
 import { AppDataSource } from "../src/data-source";
 import { Lesson, Tutorial } from "../src/entities";
+import dummyData from "../../shared/dummy_data";
 
 seedDatabase(AppDataSource);
 
@@ -14,57 +15,6 @@ export async function seedDatabase(appDataSource: DataSource) {
     // Get repositories
     const tutorialRepository = AppDataSource.getRepository(Tutorial);
     const lessonRepository = AppDataSource.getRepository(Lesson);
-
-    // Define tutorials data
-    const dummyData = [
-      {
-        title: "Introduction to HTML",
-        description:
-          "Learn how HTML is used to define the structure of web pages.",
-        is_published: true,
-        lessons: [
-          { title: "What is HTML?", is_published: true },
-          { title: "HTML Tags", is_published: true },
-          { title: "HTML Attributes", is_published: true },
-          { title: "HTML Elements", is_published: true },
-        ],
-      },
-      {
-        title: "CSS Basics",
-        description:
-          "Learn how to target DOM elements and change how they look.",
-        is_published: true,
-        lessons: [
-          { title: "What is CSS?", is_published: true },
-          { title: "CSS Selectors", is_published: true },
-          { title: "CSS Properties", is_published: true },
-          { title: "CSS Box Model", is_published: true },
-        ],
-      },
-      {
-        title: "JavaScript Fundamentals",
-        description: "Learn how to add interactivity to your web applications.",
-        is_published: true,
-        lessons: [
-          { title: "What is JavaScript?", is_published: true },
-          { title: "Variables", is_published: true },
-          { title: "Functions", is_published: true },
-          { title: "Objects", is_published: true },
-        ],
-      },
-      {
-        title: "TypeScript for Beginners",
-        description:
-          "Learn how TypeScript can help you to catch bugs and create large scale projects.",
-        is_published: true,
-        lessons: [
-          { title: "What is TypeScript?", is_published: true },
-          { title: "Type Annotations", is_published: true },
-          { title: "Interfaces", is_published: true },
-          { title: "Classes", is_published: true },
-        ],
-      },
-    ];
 
     // Create tutorials and their lessons
     for (const tutorial of dummyData) {
---
+++ shared/dummy_data.ts
@@ -0,0 +1,51 @@
+// Define tutorials data
+const dummyData = [
+  {
+    title: "Introduction to HTML",
+    description: "Learn how HTML is used to define the structure of web pages.",
+    is_published: true,
+    lessons: [
+      { title: "What is HTML?", is_published: true },
+      { title: "HTML Tags", is_published: true },
+      { title: "HTML Attributes", is_published: true },
+      { title: "HTML Elements", is_published: true },
+    ],
+  },
+  {
+    title: "CSS Basics",
+    description: "Learn how to target DOM elements and change how they look.",
+    is_published: true,
+    lessons: [
+      { title: "What is CSS?", is_published: true },
+      { title: "CSS Selectors", is_published: true },
+      { title: "CSS Properties", is_published: true },
+      { title: "CSS Box Model", is_published: true },
+    ],
+  },
+  {
+    title: "JavaScript Fundamentals",
+    description: "Learn how to add interactivity to your web applications.",
+    is_published: true,
+    lessons: [
+      { title: "What is JavaScript?", is_published: true },
+      { title: "Variables", is_published: true },
+      { title: "Functions", is_published: true },
+      { title: "Objects", is_published: true },
+    ],
+  },
+  {
+    title: "TypeScript for Beginners",
+    description:
+      "Learn how TypeScript can help you to catch bugs and create large scale projects.",
+    is_published: true,
+    lessons: [
+      { title: "What is TypeScript?", is_published: true },
+      { title: "Type Annotations", is_published: true },
+      { title: "Interfaces", is_published: true },
+      { title: "Classes", is_published: true },
+    ],
+  },
+];
+
+
+export default dummyData;
